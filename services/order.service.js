5e62471a3df4850dd4c3fe3e240f17f58e7856dd
Toni B.Lopez local <tonibassols@outlook.es>
2022-04-18 17:18:42 +0200

    Update

--- services/order.service.js
+++ services/order.service.js
@@ -1,32 +1,42 @@
 const boom = require('@hapi/boom');
 
+const { models } = require('./../libs/sequelize');
+
 class OrderService {
+	constructor() {
+	}
 
-  constructor(){
-  }
-  async create(data) {
-    return data;
-  }
+	async create(data) {
+		const newOrder = await models.Order.create(data);
+		return newOrder;
+	}
 
-  async find() {
-    return [];
-  }
+	async find() {
+		return [];
+	}
 
-  async findOne(id) {
-    return { id };
-  }
+	async findOne(id) {
+		const order = await models.Order.findByPk(id, {
+			include: [
+				{
+					association: 'customer',
+					include: ['user'],
+				},
+			],
+		});
+		return order;
+	}
 
-  async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
-  }
+	async update(id, changes) {
+		return {
+			id,
+			changes,
+		};
+	}
 
-  async delete(id) {
-    return { id };
-  }
-
+	async delete(id) {
+		return { id };
+	}
 }
 
 module.exports = OrderService;
