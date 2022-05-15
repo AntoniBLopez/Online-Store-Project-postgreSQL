72e77c05c1baee3e3b336ecd3375be3b8368aba7
Toni B.Lopez local <tonibassols@outlook.es>
2022-05-15 12:57:28 +0200

    new update

--- config/config.js
+++ config/config.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 
 const config = {
-	env: process.env.NODE_ENV || 'development',
+	env: process.env.NODE_ENV_DEV || 'development',
+  isProd: process.env.NODE_ENV_PROD || 'production',
 	port: process.env.PORT || 3000,
 	dbUser: process.env.DB_USER || 'toni',
 	dbPassword: process.env.DB_PASSWORD || 'admin123',
 	dbHost: process.env.DB_HOST || 'localhost',
-	dbPort: process.env.DB_PORT || 'my_store',
-	dbName: process.env.DB_NAME || '5432',
+	dbName: process.env.DB_NAME || 'my_store',
+	dbPort: process.env.DB_PORT || 5432,
+  dbUrl: process.env.DATABASE_URL,
 };
 
 module.exports = { config };
